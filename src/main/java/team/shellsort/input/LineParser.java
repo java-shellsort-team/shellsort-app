d385f4b700e8f6d235a9f8382d6a33984370a853
Mikhail <mikhaillat@yandex.ru>
2025-11-03 17:44:31 +0300

    feat: добавлены пакеты input и utils с файлами-заглушками

---
+++ src/main/java/team/shellsort/input/LineParser.java
@@ -0,0 +1,10 @@
+package team.shellsort.input;
+
+import team.shellsort.model.Car;
+
+public class LineParser {
+    public Car parseCar(String line) {
+        // TODO: реализовать парсинг строки вида "model;power;year"
+        return null;
+    }
+}
