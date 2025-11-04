b52afe23bdfa074194ae7e0e262988b85f05401c
Mikhail <120601272+hodkonem@users.noreply.github.com>
2025-11-04 16:38:10 +0300

    feat: добавлены заглушки sort/ и strategy/ (#8)
    Проверил необходимые папки

---
+++ src/main/java/team/shellsort/sort/ShellSort.java
@@ -0,0 +1,9 @@
+package team.shellsort.sort;
+
+import java.util.List;
+
+public class ShellSort {
+    public static <T> void sort(List<T> list, java.util.Comparator<T> cmp) {
+        // TODO: реализация
+    }
+}
