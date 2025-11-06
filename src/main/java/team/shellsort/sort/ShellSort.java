ecd01efb6b2f0e936dbe25749e2ca72699115286
faust529 <dima.mitiaev1999@yandex.ru>
2025-11-06 12:28:36 +0300

    change import

--- src/main/java/team/shellsort/sort/ShellSort.java
+++ src/main/java/team/shellsort/sort/ShellSort.java
@@ -1,9 +1,10 @@
 package team.shellsort.sort;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class ShellSort {
-    public static <T> void sort(List<T> list, java.util.Comparator<T> cmp) {
+    public static <T> void sort(List<T> list, Comparator<T> cmp) {
         // TODO: реализация
     }
 }
