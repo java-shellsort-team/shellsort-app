b96f745317477421ec53fc028227be576a8f30db
Mikhail <mikhaillat@yandex.ru>
2025-11-07 13:38:46 +0300

    feat: add SortKey enum and extend SortStrategy interface
    Added SortKey enum (BY_MODEL, BY_POWER, BY_YEAR) and updated SortStrategy with key() method for future integration with App.java menu selection.

--- src/main/java/team/shellsort/strategy/SortStrategy.java
+++ src/main/java/team/shellsort/strategy/SortStrategy.java
@@ -1,10 +1,17 @@
 package team.shellsort.strategy;
 
 import team.shellsort.model.Car;
+
 import java.util.Comparator;
 
+/**
+ * Общий контракт для стратегий сортировки.
+ * Возвращает Comparator<Car> и связан с SortKey.
+ */
 public interface SortStrategy {
     Comparator<Car> comparator();
-    String name();
-}
 
+    String name();
+
+    SortKey key();
+}
